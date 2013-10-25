37921ad4a7121d9f78b248b448f98c8dd1301755
Ethan Stryker <e.stryker@gmail.com>
2013-10-25 11:07:36 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mobilemap::Application.config.secret_token = '4e4768da2322ed6283d0a0a2f66149d72c2a811e45201f3a52c1fae934ff7a07c614c079bfffc9baf25f1c1ce9207f289a66c3cb7c12b234f9e095f406eb7290'
