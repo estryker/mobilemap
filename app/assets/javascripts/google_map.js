c6b17ac8b4a14d7e03272c2833161377c425aa17
Ethan Stryker <e.stryker@gmail.com>
2014-01-20 12:37:12 -0500

    added user geolocation callback

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -3,6 +3,7 @@
 */
 
 var map;
+var user_coordinates; 
 
 function createEventMarker(eventLatlng) {
     return new google.maps.Marker({position: eventLatlng, map: map});
@@ -24,3 +25,42 @@ function setupMap(lat, lng, mapZoom, showOverviewControl) {
     };
     map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 }
+
+/*
+These three functions are given to navigator.geolocation.getCurrentPosition
+so that we know how to handle success, error and to give options
+*/
+var geolocation_options = {
+  enableHighAccuracy: true,
+  timeout: 15000,
+  maximumAge: 0
+};
+
+function geolocation_success(pos) {
+  user_coordinates = pos.coords;
+
+  console.log('Your current position is:');
+  console.log('Latitude : ' + user_coordinates.latitude);
+  console.log('Longitude: ' + user_coordinates.longitude);
+  console.log('More or less ' + user_coordinates.accuracy + ' meters.');
+  setupMap(user_coordinates.latitude, user_coordinates.longitude, 11, true);
+};
+
+function geolocation_error(err) {
+  user_coordinates = new google.maps.LatLng(39.3, -76.616);
+  console.warn('ERROR(' + err.code + '): ' + err.message); 
+};
+
+function initialize() {
+    // need a good default location ...
+    setupMap(39.3, -76.616, 11, true);
+
+    // non-blocking, so we set up the map first, then we will move
+    // to this location if/when we get a position
+    navigator.geolocation.getCurrentPosition(geolocation_success, geolocation_error, geolocation_options);
+    //    var eventLatlng = new google.maps.LatLng(39.3, -76.616);
+    // var marker = createEventMarker(eventLatlng);
+    //marker.setAnimation(google.maps.Animation.DROP);
+    // $('#map_canvas').gmap({'center': marker});
+};
+
