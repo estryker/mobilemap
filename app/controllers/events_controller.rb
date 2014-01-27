40c78fd449c432105faa831799ec97630b2d6e5f
Ethan Stryker <e.stryker@gmail.com>
2014-01-26 23:04:00 -0500

    updated about and settings pages to be dialog and internal pages

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -101,8 +101,11 @@ class EventsController < ApplicationController
 
     respond_to do |format|
       if @event.save
-        #format.html { redirect_to @event, notice: 'Event was successfully created.' }
-        format.html {redirect_to root_path }
+        $stderr.puts "event save"
+        $stderr.puts root_path
+        format.html { redirect_to root_path, notice: 'Event was successfully created.' }
+        # format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        # format.html { redirect_to root_path }
         format.json { render json: @event, status: :created, location: @event }
       else
         format.html { render action: "new" }
